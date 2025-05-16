700f92a9837883528f2cbc369e74748e3ef62cef
o.tararenko@beloil.by <o.tararenko@beloil.by>
2025-05-16 15:48:54 +0300

    before geting uuid for Viewer

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -12,30 +12,45 @@ sap.ui.define([
 
         onInit: function () {
 
-           
+            this.extensionAPI.attachPageDataLoaded(this.onDetailsLoaded, this);
 
-            this._sValidPath = sap.ui.require.toUrl("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
+          //  this._sValidPath = sap.ui.require.toUrl("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
             //this._sInvalidPath = sap.ui.require.toUrl("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
 
+           
+            var url1 = "/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%27";
+            var url2 = "%27)/$value";
             var oModeltest = new JSONModel();
             oModeltest.setData({
-                Source: this._sValidPath,
+                Source: "",
                 Title: "My Custom Title",
                 Height: "600px"
             });
-            var sUsername = "o.tararenko";
+          //  var sUsername = "o.tararenko";
            
-            var sPassword = "Oksana33";
-            var sAuthHeader = "Basic " + btoa(sUsername + ":" + sPassword);
-            //var test = this.getView().byId("idViewpdf");
-            this.getView().byId("idViewpdf").setModel(oModeltest);
-           // this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value")
+           // var sPassword = "Oksana33";
+          //  var sAuthHeader = "Basic " + btoa(sUsername + ":" + sPassword);
+            
+            var urluuid = window.location.href;
+            var aWords = urluuid.split("'"); 
+           /* if ( aWords[1].length > 0 ) { 
+                var stext = aWords[1].toUpperCase();
+                var surlnew = url1 + stext.replace(/-/g, "") + url2;
+           
+             }*/
+             this.getView().byId("idViewpdf").setModel(oModeltest);
+            this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
+            
+            
+            
+
+          
 
 
          
 
 
-            fetch("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value", {
+          /*  fetch("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value", {
                 method: "GET",
                 headers: {
                     "Authorization": sAuthHeader
@@ -72,7 +87,7 @@ sap.ui.define([
                 .catch(error => {
                     console.error("Ошибка:", error)
                 }
-                );
+                );*/
 
         },
         base64toBlob: function (base64, contentType = 'application/pdf') {
@@ -83,7 +98,48 @@ sap.ui.define([
             }
             const byteArray = new Uint8Array(byteNumbers);
             return new Blob([byteArray], { type: contentType });
+          },
+
+        initPdfViewer: function (uuid) {
+            var oModeltest = new JSONModel();
+            oModeltest.setData({
+                Source: this._sValidPath,
+                Title: "My Custom Title",
+                Height: "600px"
+            });
+         
+            this.getView().byId("idViewpdf").setModel(oModeltest);
+            this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value")
+          }.bind(this),
+
+    
+
+
+
+          onBeforeRendering: function () {
+            console.log('onBeforeRendering');
+          },
+       
+          onAfterRendering: function () {
+            //this.byId("listReportFilter-btnGo").firePress();
+          //  initPdfViewer("3");
+            console.log('onAfterRendering');
+          },
+       
+          onValueHelpRequest: function (event) {
+            var sPath = event.context.sPath; // has the context path
+            var contextObj = event.context.getObject(); //contains the context Object and has all the properties bound in the list report Item
+          },
+       
+          onDetailsLoaded: function (event, this1) {
+           // MessageToast.show('');
+            var sPath = event.context.sPath; // has the context path
+       
+            var contextObj = event.context.getObject(); //contains the context Object and has all the properties bound in the list report Item
+           // initPdfViewer("2");
           }
+
+          
 
 
 
