7c0f99b8646db3fe3ee9524d212ae989d8725d3f
o.tararenko@beloil.by <o.tararenko@beloil.by>
2025-05-19 09:20:13 +0300

    19052025-code optimized - unnecessary elements removed

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -13,109 +13,32 @@ sap.ui.define([
         onInit: function () {
 
             this.extensionAPI.attachPageDataLoaded(this.onDetailsLoaded, this);
-
-          //  this._sValidPath = sap.ui.require.toUrl("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
-            //this._sInvalidPath = sap.ui.require.toUrl("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
-
-           
+ 
             var url1 = "/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%27";
             var url2 = "%27)/$value";
             var oModeltest = new JSONModel();
             oModeltest.setData({
                 Source: "",
-                Title: "My Custom Title",
+                Title: "Просмотр файла",
                 Height: "600px"
             });
-          //  var sUsername = "o.tararenko";
-           
-           // var sPassword = "Oksana33";
-          //  var sAuthHeader = "Basic " + btoa(sUsername + ":" + sPassword);
-            
+ 
             var urluuid = window.location.href;
-            var aWords = urluuid.split("'"); 
-           /* if ( aWords[1].length > 0 ) { 
-                var stext = aWords[1].toUpperCase();
-                var surlnew = url1 + stext.replace(/-/g, "") + url2;
-           
-             }*/
-             this.getView().byId("idViewpdf").setModel(oModeltest);
-            this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value");
-            
-            
-            
+            var searchWord = "uuid=guid";
 
-          
-
-
-         
-
-
-          /*  fetch("https://sapbpc-dev.beloil.by/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value", {
-                method: "GET",
-                headers: {
-                    "Authorization": sAuthHeader
-                }
-            })
-                .then(response => response.blob())
-                .then(blob => {
-                    const url = URL.createObjectURL(blob);
-
-                    var reader = new FileReader();
-                    reader.readAsDataURL(blob);
-                    reader.onloadend = function () {
-                    var base64data = reader.result;
-                   
-                   this.byId("idViewpdf").byId("_IDGenPDFViewer").setSource(base64data);
-                   
-
-                  
-
-                   // this.getView().byId("idViewpdf").setModel(oModeltest1);
-                    //this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource(base64data)
-                       // pdfViewer.setSource(base64data);
-                     }.bind(this);
-                      
-                   
-                   
-
-                  //this.getView().byId("idViewpdf").setModel(oModeltest1);
-             //     this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource(url)
-                  
-                 
-                })
-
-                .catch(error => {
-                    console.error("Ошибка:", error)
-                }
-                );*/
-
-        },
-        base64toBlob: function (base64, contentType = 'application/pdf') {
-            const byteCharacters = atob(base64);
-            const byteNumbers = new Array(byteCharacters.length);
-            for (let i = 0; i < byteCharacters.length; i++) {
-              byteNumbers[i] = byteCharacters.charCodeAt(i);
+            var index = urluuid.indexOf(searchWord);
+            if (index != -1) {
+                var indexstr = urluuid.substring(index, index+47);
+                var splitstr = indexstr.split("'")
+                if ( splitstr.length  > 0 ){
+                    var surlnew = url1 + splitstr[1].toUpperCase().replace(/-/g, "") + url2;
+                    this.getView().byId("idViewpdf").setModel(oModeltest);
+                    this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource(surlnew);
+                    }
+              
             }
-            const byteArray = new Uint8Array(byteNumbers);
-            return new Blob([byteArray], { type: contentType });
-          },
-
-        initPdfViewer: function (uuid) {
-            var oModeltest = new JSONModel();
-            oModeltest.setData({
-                Source: this._sValidPath,
-                Title: "My Custom Title",
-                Height: "600px"
-            });
-         
-            this.getView().byId("idViewpdf").setModel(oModeltest);
-            this.getView().byId("idViewpdf").byId("_IDGenPDFViewer").setSource("/sap/opu/odata/sap/ZCV_ATTACHMENT_SRV_SRV/zfilemSet(%2700505691A5F21FD08AE25DA7D1C50436%27)/$value")
-          }.bind(this),
-
+        },
     
-
-
-
           onBeforeRendering: function () {
             console.log('onBeforeRendering');
           },
@@ -138,11 +61,6 @@ sap.ui.define([
             var contextObj = event.context.getObject(); //contains the context Object and has all the properties bound in the list report Item
            // initPdfViewer("2");
           }
-
-          
-
-
-
 
     };
 });
